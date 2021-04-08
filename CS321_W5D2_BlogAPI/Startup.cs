6ca2fe9b36910ca846f065b2b7c5b8dd8edf3cff
jordan.woodruff <jordan.woodruff@tylertech.com>
2021-04-07 20:38:15 -0500

    netcoreapp3.1...again

--- CS321_W5D2_BlogAPI/Startup.cs
+++ CS321_W5D2_BlogAPI/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 
 namespace CS321_W5D2_BlogAPI
@@ -27,7 +28,7 @@ namespace CS321_W5D2_BlogAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc();
 
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
@@ -57,7 +58,7 @@ namespace CS321_W5D2_BlogAPI
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env /*, DbInitializer dbInitializer*/)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env /*, DbInitializer dbInitializer*/)
         {
             if (env.IsDevelopment())
             {
